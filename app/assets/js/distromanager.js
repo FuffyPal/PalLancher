c89a2cb2360699bc0b96f37e8fddf5d81741687f
Fluffy Pal <email@email.com>
2026-03-14 14:01:52 +0300

    awfwa

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -1,17 +1,17 @@
-const { DistributionAPI } = require('helios-core/common')
+const { DistributionAPI } = require("helios-core/common");
 
-const ConfigManager = require('./configmanager')
+const ConfigManager = require("./configmanager");
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'https://helios-files.geekcorner.eu.org/distribution.json'
+exports.REMOTE_DISTRO_URL = "https://gist.githubusercontent.com/FuffyPal/9e1dc4dca4d2f780acd2efa8659b9c96/raw/b9668a71da253dde0fa4505d5b0239520a388f2a/gistfile1.txt"
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
     null, // Injected forcefully by the preloader.
     null, // Injected forcefully by the preloader.
     exports.REMOTE_DISTRO_URL,
-    false
-)
+    false,
+);
 
-exports.DistroAPI = api
+exports.DistroAPI = api;
